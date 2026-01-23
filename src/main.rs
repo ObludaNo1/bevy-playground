c414bc0a83595433a2bacdf3a60e21b1560c125a
Ondřej Caha <cahaondra@gmail.com>
2026-01-23 18:50:14 +0900

    WIP

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
-fn main() {
+// use bevy::prelude::*;
+
+// #[derive(Component)]
+// struct Name(String);
+
+fn hello_world() {
     println!("Hello, world!");
+}
+
+// fn add_people(mut commands: Commands) {
+//     commands.spawn((Person, Name("Elaina Proctor".to_string())));
+//     commands.spawn((Person, Name("Renzo Hume".to_string())));
+//     commands.spawn((Person, Name("Zayna Nieves".to_string())));
+// }
+
+// fn main() {
+//     fn main() {
+//         App::new()
+//             .add_systems(Startup, add_people)
+//             .add_systems(Update, hello_world)
+//             .run();
+//     }
+// }
+fn main() {
+    hello_world();
 }
