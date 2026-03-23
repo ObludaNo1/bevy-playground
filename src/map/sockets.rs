4ebebe25fcad6c1b555560ba397ad81a4a763fbc
Ondřej Caha <cahaondra@gmail.com>
2026-03-23 11:23:56 +0900

    Added grass layers

--- src/map/sockets.rs
+++ src/map/sockets.rs
@@ -2,12 +2,23 @@ use bevy_procedural_tilemaps::prelude::*;
 
 pub struct TerrainSockets {
     pub dirt: DirtLayerSockets,
+    pub void: Socket,             // line update alert
+    pub grass: GrassLayerSockets, // line update alert
 }
 
 pub struct DirtLayerSockets {
     pub layer_up: Socket,   // What can sit on top of dirt
     pub layer_down: Socket, // What dirt can sit on
     pub material: Socket,   // What dirt connects to horizontally
+}
+
+pub struct GrassLayerSockets {
+    pub layer_up: Socket,
+    pub layer_down: Socket,
+    pub material: Socket,
+    pub void_and_grass: Socket,
+    pub grass_and_void: Socket,
+    pub grass_fill_up: Socket,
 }
 
 pub fn create_sockets(socket_collection: &mut SocketCollection) -> TerrainSockets {
@@ -18,6 +29,17 @@ pub fn create_sockets(socket_collection: &mut SocketCollection) -> TerrainSocket
             layer_up: new_socket(),
             material: new_socket(),
             layer_down: new_socket(),
+        },
+        // line update alert
+        void: new_socket(),
+        // lines update alert
+        grass: GrassLayerSockets {
+            layer_up: new_socket(),
+            material: new_socket(),
+            layer_down: new_socket(),
+            void_and_grass: new_socket(),
+            grass_and_void: new_socket(),
+            grass_fill_up: new_socket(),
         },
     };
     sockets
